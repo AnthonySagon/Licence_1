d2484c976352a3073c085a2d4eb198ee4662c6e0
Anthony Sagon <sagon.anthony@gmail.com>
2021-06-27 18:40:40 +0200

    Ex1.3 Q.1

--- Exos_Numeriques_Simples.py
+++ Exos_Numeriques_Simples.py
@@ -69,3 +69,28 @@ assert prix_ht(100, 0) == 100.0
 assert prix_ht(200, 100) == 100.0
 assert prix_ht(0, 20) == 0.0
 assert prix_ht(211, 5.5) == 200.0
+
+"""Exercice 1.3 : Calcul de fonctions polynômiales 
+
+Question 1
+Après avoir spécifié le problème, écrire un jeu de tests et donner une définition de la fonction
+polynomiale telle que polynomiale(a, b, c, d, x) évalue le polynôme ax3 + bx2 + cx + d
+(à coefficients flottants)."""
+
+def polynomiale(a : float, b : float, c : float, d : float, x : float) -> float:
+    """Retourne la valeur de a*x^3 + b*x^2 + c*x + d
+    """
+    return (a*x*x*x + b*x*x + c*x + d)
+# remarque : on peut aussi utiliser la fonction puissance :
+# return (a*x**3 + b*x**2 + c*x + d)
+# mais en considèrant que l'opérateur x**3 effectue lui-même x*x*x
+# cela revient à faire le même nombre de multiplications.
+
+# Jeu de tests
+assert polynomiale(1,1,1,1,2) == 15
+assert polynomiale(1,1,1,1,3) == 40
+assert polynomiale(2,0,0,0,1) == 2
+assert polynomiale(0,3,0,0,1) == 3
+assert polynomiale(0,0,4,0,1) == 4
+assert polynomiale(1,2,3,4,0) == 4
+assert polynomiale(2,3,4,5,1) == 14
