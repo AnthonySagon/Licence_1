6a1a3e60ec9d13f32ba6ca79c970c4431828d6e8
Anthony Sagon <sagon.anthony@gmail.com>
2021-06-27 20:19:16 +0200

    Ex1.5 Q.2

--- Exos_Numeriques_Simples.py
+++ Exos_Numeriques_Simples.py
@@ -1,15 +1,19 @@
+import math
+
 """Exercice 1.1 : Moyenne de trois nombres
 
 Question 1
 Donner une définition de la fonction moyenne_trois_nb qui effectue la moyenne arithmétique
 de trois nombres."""
 
-def moyenne_trois_nb(a : float, b : float, c : float) -> float:
+
+def moyenne_trois_nb(a: float, b: float, c: float) -> float:
     """Retourne la moyenne arithmétique de trois nombres a, b et c.
     """
-    return (a + b + c)/3.0 #Division flottante
+    return (a + b + c)/3.0  # Division flottante
 
-# Jeu de tests
+
+# Jeu de test
 assert moyenne_trois_nb(3, 6, -3) == 2.0
 assert moyenne_trois_nb(1.5, 2.5, 1.0) == 5.0 / 3.0
 assert moyenne_trois_nb(3, 6, 3) == 4.0
@@ -20,11 +24,13 @@ assert moyenne_trois_nb(1, 1, 1) == 1.0
 a, b, c avec des coefficients de pondération, respectivement pa (pondération en a), pb et pc.
 Proposer un jeu de tests comprenant au moins trois tests."""
 
-def moyenne_ponderee(a : float, b : float, c : float, pa : float, pb : float, pc : float) -> float:
+
+def moyenne_ponderee(a: float, b: float, c: float, pa: float, pb: float, pc: float) -> float:
     """Précondition : pa + pb + pc != 0
     Retourne la moyenne des trois nombres a, b, c, pondérés respectivement par pa (pondération pour a), pb et pc.
     """
     return ((a * pa) + (b * pb) + (c * pc)) / (pa + pb + pc)
+
 
 # Jeu de tests
 assert moyenne_ponderee(1, 1, 1, 3, 6, -3) == 1.0
@@ -39,12 +45,14 @@ Ecrire une définition de la fonction prix_ttc qui calcule le prix toutes taxes 
 à partir d’un prix hors taxe (HT) et d’un taux de TVA exprimé en pourcentage, par exemple
 20.0 pour une TVA de 20%."""
 
-def prix_ttc(prix : float, taux : float) -> float:
+
+def prix_ttc(prix: float, taux: float) -> float:
     """Précondition : prix >= 0
     Retourne le prix TTC correspondant au prix HT 'prix'
     avec un taux de TVA 'taux'
     """
     return prix * (1 + taux / 100.0)
+
 
 # Jeu de tests
 assert prix_ttc(100.0, 20.0) == 120.0
@@ -57,11 +65,13 @@ assert prix_ttc(200, 5.5) == 211.0
 Donner une définition de la fonction prix_ht qui calcule le prix hors taxe à partir du prix toutes
 taxes comprises et du taux de TVA."""
 
-def prix_ht(prix : float, taux : float) -> float:
+
+def prix_ht(prix: float, taux: float) -> float:
     """Précondition : prix >= 0
     Retourne le prix HT correspondant au prix TTC 'prix'
     avec un taux de TVA 'taux'"""
     return prix / (1 + taux / 100.0)
+
 
 # Jeu de tests
 assert prix_ht(120, 20) == 100.0
@@ -77,71 +87,79 @@ Après avoir spécifié le problème, écrire un jeu de tests et donner une déf
 polynomiale telle que polynomiale(a, b, c, d, x) évalue le polynôme ax3 + bx2 + cx + d
 (à coefficients flottants)."""
 
-def polynomiale(a : float, b : float, c : float, d : float, x : float) -> float:
+
+def polynomiale(a: float, b: float, c: float, d: float, x: float) -> float:
     """Retourne la valeur de a*x^3 + b*x^2 + c*x + d
     """
-    return (a*x*x*x + b*x*x + c*x + d)
+    return a*x*x*x + b*x*x + c*x + d
 # remarque : on peut aussi utiliser la fonction puissance :
 # return (a*x**3 + b*x**2 + c*x + d)
 # mais en considèrant que l'opérateur x**3 effectue lui-même x*x*x
 # cela revient à faire le même nombre de multiplications.
 
+
 # Jeu de tests
-assert polynomiale(1,1,1,1,2) == 15
-assert polynomiale(1,1,1,1,3) == 40
-assert polynomiale(2,0,0,0,1) == 2
-assert polynomiale(0,3,0,0,1) == 3
-assert polynomiale(0,0,4,0,1) == 4
-assert polynomiale(1,2,3,4,0) == 4
-assert polynomiale(2,3,4,5,1) == 14
+assert polynomiale(1, 1, 1, 1, 2) == 15
+assert polynomiale(1, 1, 1, 1, 3) == 40
+assert polynomiale(2, 0, 0, 0, 1) == 2
+assert polynomiale(0, 3, 0, 0, 1) == 3
+assert polynomiale(0, 0, 4, 0, 1) == 4
+assert polynomiale(1, 2, 3, 4, 0) == 4
+assert polynomiale(2, 3, 4, 5, 1) == 14
+
 
 # Plus efficace :
-def polynomiale_2(a : float, b : float, c : float, d : float, x : float) -> float:
+def polynomiale_2(a: float, b: float, c: float, d: float, x: float) -> float:
     """Retourne la valeur de a*x^3 + b*x^2 + c*x + d
     """
-    return (((((a*x + b) * x) + c) * x) + d)
+    return ((((a*x + b) * x) + c) * x) + d
+
 
 # Jeu de tests
-assert polynomiale_2(1,1,1,1,2) == 15
-assert polynomiale_2(1,1,1,1,3) == 40
-assert polynomiale_2(2,0,0,0,1) == 2
-assert polynomiale_2(0,3,0,0,1) == 3
-assert polynomiale_2(0,0,4,0,1) == 4
-assert polynomiale_2(1,2,3,4,0) == 4
-assert polynomiale_2(2,3,4,5,1) == 14
+assert polynomiale_2(1, 1, 1, 1, 2) == 15
+assert polynomiale_2(1, 1, 1, 1, 3) == 40
+assert polynomiale_2(2, 0, 0, 0, 1) == 2
+assert polynomiale_2(0, 3, 0, 0, 1) == 3
+assert polynomiale_2(0, 0, 4, 0, 1) == 4
+assert polynomiale_2(1, 2, 3, 4, 0) == 4
+assert polynomiale_2(2, 3, 4, 5, 1) == 14
 
 """Question 2
 Après avoir spécifié le problème, écrire un jeu de tests et donner une définition de la fonction
 polynomiale_carre qui rend la valeur de ax^4 + bx^2 + c."""
 
-def polynomiale_carre(a : float, b : float, c : float, x : float) -> float:
+
+def polynomiale_carre(a: float, b: float, c: float, x: float) -> float:
     """Retourne la valeur de ax^4 + bx^2 + c
     """
-    return (a*x*x*x*x + b*x*x + c)
+    return a*x*x*x*x + b*x*x + c
 # ou
 # return (a*x**4 + b*x**2 + c)
 
+
 # Jeu de tests
-assert polynomiale_carre(1,1,1,2) == 21
-assert polynomiale_carre(1,1,1,3) == 91
-assert polynomiale_carre(2,0,0,1) == 2
-assert polynomiale_carre(0,3,0,1) == 3
-assert polynomiale_carre(2,3,4,0) == 4
-assert polynomiale_carre(2,3,4,1) == 9
+assert polynomiale_carre(1, 1, 1, 2) == 21
+assert polynomiale_carre(1, 1, 1, 3) == 91
+assert polynomiale_carre(2, 0, 0, 1) == 2
+assert polynomiale_carre(0, 3, 0, 1) == 3
+assert polynomiale_carre(2, 3, 4, 0) == 4
+assert polynomiale_carre(2, 3, 4, 1) == 9
+
 
 # Plus efficace (schéma de Hörner) :
-def polynomial_carre(a : float, b : float, c : float, x : float) -> float:
+def polynomial_carre(a: float, b: float, c: float, x: float) -> float:
     """Retourne la valeur de ax^4 + bx^2 + c
     """
-    return (((a*x*x + b) * x*x) + c)
+    return ((a*x*x + b) * x*x) + c
+
 
 # Jeu de tests
-assert polynomial_carre(1,1,1,2) == 21
-assert polynomial_carre(1,1,1,3) == 91
-assert polynomial_carre(2,0,0,1) == 2
-assert polynomial_carre(0,3,0,1) == 3
-assert polynomial_carre(2,3,4,0) == 4
-assert polynomial_carre(2,3,4,1) == 9
+assert polynomial_carre(1, 1, 1, 2) == 21
+assert polynomial_carre(1, 1, 1, 3) == 91
+assert polynomial_carre(2, 0, 0, 1) == 2
+assert polynomial_carre(0, 3, 0, 1) == 3
+assert polynomial_carre(2, 3, 4, 0) == 4
+assert polynomial_carre(2, 3, 4, 1) == 9
 
 """Exercice 1.4 : Aire d’une couronne
 Cet exercice a pour but de calculer l’aire d’une couronne (c’est-à-dire l’aire comprise entre 2
@@ -153,13 +171,14 @@ d’un disque de rayon r
 Remarque : En python, la constante π est déjà définie dans le module math. Pour l’utiliser, il
 faut déclarer l’utilisation de ce module en tête du programme avec l’instruction suivante :
 """
-import math
 
-def aire_disque(r : float) -> float:
+
+def aire_disque(r: float) -> float:
     """Précondition : r>0
     Retourne l’aire πr2 d’un disque de rayon r
     """
     return math.pi * r*r
+
 
 # Jeu de tests :
 assert 3.14 < aire_disque(1.0) < 3.15
@@ -172,11 +191,13 @@ deux nombres r1 et r2, calcule l’aire de la couronne de rayon intérieur r1 et
 r2.
 Par hypothèse, on considère que le rayon intérieur est inférieur ou égal au rayon extérieur."""
 
-def aire_couronne(r1 : float, r2 : float) -> float:
+
+def aire_couronne(r1: float, r2: float) -> float:
     """Précondition : 0 < r1 <= r2
     Retourne l’aire de la couronne de rayon intérieur r1 et de rayon extérieur r2.
     """
     return math.pi * (r2*r2 - r1*r1)
+
 
 # Jeu de tests :
 assert 3.14 < aire_couronne(0.0, 1.0) < 3.15
@@ -195,14 +216,30 @@ exprimée en degrés Fahrenheit en son équivalent en degrés Celsius.
 Rappel : la température t en degrés Fahrenheit équivaut à la température (t − 32) ∗ 5/9 en degrés Celsius.
 """
 
-def fahrenheit_vers_celsius(t : float) -> float:
+
+def fahrenheit_vers_celsius(t: float) -> float:
     """convertit une température t exprimée en degrés Fahrenheit en son équivalent en degrés Celsius
     """
     return (t - 32) * 5/9
 
+
 # Jeu de tests :
-assert fahrenheit_vers_celsius(212) == 100.0
-assert fahrenheit_vers_celsius(32) == 0.0
-assert fahrenheit_vers_celsius(41) == 5.0
+assert fahrenheit_vers_celsius(212.0) == 100.0
+assert fahrenheit_vers_celsius(32.0) == 0.0
+assert fahrenheit_vers_celsius(41.0) == 5.0
+
+"""Question 2
+Donner une définition de la fonction celsius_vers_fahrenheit qui effectue la conversion
+inverse."""
 
 
+def celsius_vers_fahrenheit(t: float) -> float:
+    """convertit une température t exprimée en degrés Celsius en son équivalent en degrés Fahrenheit
+    """
+    return t * 9/5 + 32
+
+
+# Jeu de tests :
+assert celsius_vers_fahrenheit(100.0) == 212.00
+assert celsius_vers_fahrenheit(0.0) == 32.0
+assert celsius_vers_fahrenheit(5.0) == 41.0
