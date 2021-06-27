1824da9e0c39f616eaa3ac930cca76277aaa816b
Anthony Sagon <sagon.anthony@gmail.com>
2021-06-27 14:20:05 +0200

    Ex1.2 Q.1

--- Exos_Numeriques_Simples.py
+++ Exos_Numeriques_Simples.py
@@ -52,3 +52,20 @@ assert prix_ttc(100, 0.0) == 100.0
 assert prix_ttc(100, 100.0) == 200.0
 assert prix_ttc(0, 20) == 0.0
 assert prix_ttc(200, 5.5) == 211.0
+
+"""Question 2
+Donner une définition de la fonction prix_ht qui calcule le prix hors taxe à partir du prix toutes
+taxes comprises et du taux de TVA."""
+
+def prix_ht(prix : float, taux : float) -> float:
+    """Précondition : prix >= 0
+    Retourne le prix HT correspondant au prix TTC 'prix'
+    avec un taux de TVA 'taux'"""
+    return prix / (1 + taux / 100.0)
+
+# Jeu de tests
+assert prix_ht(120, 20) == 100.0
+assert prix_ht(100, 0) == 100.0
+assert prix_ht(200, 100) == 100.0
+assert prix_ht(0, 20) == 0.0
+assert prix_ht(211, 5.5) == 200.0
